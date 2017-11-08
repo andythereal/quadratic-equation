8d3eb4193a0f8bf69b20a8becbe3d20eca83e347
Andriy Levkiv <andy.levkiv@gmail.com>
2017-11-08 05:31:52 +0000

    2nd

--- js/index.js
+++ js/index.js
@@ -9,16 +9,16 @@ else {
     if (disk > 0) {
         const x1 = (-1 * b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
         const x2 = (-1 * b - Math.sqrt(b * b - 4 * a * c)) / 2 * a;
-        document.write('Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
-        document.write('<br>' + '<br>' + 'Result: x1 = ' + x1 + '<br>' + 'Result: x2 = ' + x2);
+        document.write('<br>' + 'Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
+        document.write('<br>' + '<br>' + 'Result: x1 = ' + x1 + '<br>' + 'Result: x2 = ' + x2 + '<br>');
     }
     else if (disk == 0) {
         const x = (-1 * b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
-        document.write('Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
-        document.write('<br>' + '<br>' + 'Result: x1 = ' + x);
+        document.write('<br>' + 'Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
+        document.write('<br>' + '<br>' + 'Result: x1 = ' + x + '<br>');
     }
     else {
-        document.write('Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
-        document.write('<br>' + '<br>' + 'This equation has no real solution !');
+        document.write('<br>' + 'Your Equation: ' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
+        document.write('<br>' + '<br>' + 'This equation has no real solution !' + '<br>' + '<br>');
     }
 }
